5067b63508dcdea54fcab1338e07154563af5226
“maki5588” <your-mail@example.com>
2020-08-26 00:01:23 +0900

    fix

--- src/video_information_scraper.py
+++ src/video_information_scraper.py
@@ -29,12 +29,6 @@ class ChannelCountryAndScraper(object):
         self.video_urls = []
         self.nihongo_channel_countries = []
         self.channel_subscribers_true = []
-        self.views = []
-        self.create_ats = []
-        self.tag_str = []
-        self.descriptions = []
-        self.likes = []
-        self.dislikes = []
         self.search_data_csv_file_name = "./../data/search/youtube_search_csv_data"
         self.search_data_csv_file_path = os.path.join(os.getcwd(), self.search_data_csv_file_name+'.csv')
         self.scarch_videos_list_csv_file_name = "./../data/search/scarch_videos_list_scv"
@@ -46,8 +40,7 @@ class ChannelCountryAndScraper(object):
         self.csv_file_drop_duplicate()
         self.read_csv_urls()
         self.get_page_source()
-        self.add_as_csv_file()
-        self.csv_file_drop_duplicate()
+        # self.add_as_csv_file()
 
 
     def copy_csv(self):
@@ -57,15 +50,16 @@ class ChannelCountryAndScraper(object):
 
 
     def csv_file_drop_duplicate(self):
-        df = pd.read_csv(self.scarch_videos_list_csv_file_path)
+        df = pd.read_csv(self.scarch_videos_list_csv_file_path, engine='python')
         df_drop_duplicate = df.drop_duplicates(subset='video_url', keep='last')
         df_add_csv = pd.DataFrame(df_drop_duplicate).to_csv(self.scarch_videos_list_csv_file_path,index=False)
         print(self.scarch_videos_list_csv_file_path+"重複動画削除")
 
 
     def read_csv_urls(self):
-        df = pd.read_csv(self.scarch_videos_list_csv_file_path,index_col='video_url')
-        video_url_data = df.index.values
+        self.df = pd.read_csv(self.scarch_videos_list_csv_file_path, engine='python')
+        channel_url_data = self.df.set_index('video_url')
+        video_url_data = channel_url_data.index.values
         video_urls = video_url_data.tolist()
         for i in video_urls:
             youtube_url = 'https://www.youtube.com'
@@ -79,15 +73,22 @@ class ChannelCountryAndScraper(object):
             print(i)
             html = requests.get('http://localhost:8050/render.html',
             params={'url': i, 'wait': 2})
+            self.video_url_i = i
             self.soup = BeautifulSoup(html.text, "html.parser")
             self.parse_view_and_createAt()
             self.parse_video_tags()
             self.parse_video_description()
             self.parse_video_like()
             self.only_last_tags()
+            self.scarch_videos_list_csv_scarch_column()
+            self.add_as_csv_file()
+            self.scarch_videos_list_csv_duplicate()
 
 
     def parse_view_and_createAt(self):
+        self.views = []
+        self.create_ats = []
+        self.tag_str = []
         for i in self.soup.find_all('div', {"class" : "style-scope ytd-video-primary-info-renderer"}):
             view_i = re.findall('</ytd-badge-supported-renderer><div class="style-scope ytd-video-primary-info-renderer" id="info"><div class="style-scope ytd-video-primary-info-renderer" id="info-text"><div class="style-scope ytd-video-primary-info-renderer" id="count"><yt-view-count-renderer class="style-scope ytd-video-primary-info-renderer" small_=""><!--css-build:shady--><span class="view-count style-scope yt-view-count-renderer">.*</span><span class="short-view-count style-scope yt-view-count-renderer">', str(i))
             view_i_join = ",".join(view_i)
@@ -138,6 +139,7 @@ class ChannelCountryAndScraper(object):
 
 
     def parse_video_description(self):
+        self.descriptions = []
         for i in self.soup.find_all('meta'):
             description_i = re.findall('<meta content.*property="og:description"/>', str(i))
             description_i_join = ",".join(description_i)
@@ -151,6 +153,8 @@ class ChannelCountryAndScraper(object):
 
 
     def parse_video_like(self):
+        self.likes = []
+        self.dislikes = []
         for i in self.soup.find_all('yt-formatted-string'):
             like_i = re.findall('aria-label.* likes', str(i))
             like_i_join = ",".join(like_i)
@@ -172,15 +176,78 @@ class ChannelCountryAndScraper(object):
                 print(self.dislikes)
 
 
+    # def add_as_csv_file(self):
+    #     df = pd.read_csv(self.scarch_videos_list_csv_file_path)
+    #     df['view'] = self.views
+    #     df['create_at'] = self.create_ats
+    #     df['tag'] = self.tag_str
+    #     df['description'] = self.descriptions
+    #     df['like'] = self.likes
+    #     df['dislike'] = self.dislikes
+    #     df_add = pd.DataFrame(df).to_csv(self.scarch_videos_list_csv_file_path, mode='a', header=False,index=False)
+
+
+    def scarch_videos_list_csv_scarch_column(self):
+        video_url_i = self.video_url_i
+        video_url = video_url_i.replace('https://www.youtube.com', '')
+        print(video_url)
+        mask = self.df['video_url'] == video_url
+        self.true_column = self.df[mask]
+
+
     def add_as_csv_file(self):
-        df = pd.read_csv(self.scarch_videos_list_csv_file_path)
-        df['view'] = self.views
-        df['create_at'] = self.create_ats
-        df['tag'] = self.tag_str
-        df['description'] = self.descriptions
-        df['like'] = self.likes
-        df['dislike'] = self.dislikes
-        df_add = pd.DataFrame(df).to_csv(self.scarch_videos_list_csv_file_path, mode='a', header=False,index=False)
+        try:
+            self.true_column['view'] = self.views
+        except AttributeError:
+            print(self.views)
+            self.true_column['view'] = "エラー"
+        except ValueError:
+            print(self.views)
+            self.true_column['view'] = "エラー"
+        try:
+            self.true_column['create_at'] = self.create_ats
+        except AttributeError:
+            print(self.create_ats)
+            self.true_column['create_at'] = "エラー"
+        except ValueError:
+            print(self.channel_length)
+            self.true_column['create_at'] = "エラー"
+        try:
+            self.true_column['tag'] = self.tag_str
+        except AttributeError:
+            print(self.tag_str)
+            self.true_column['tag'] = "エラー"
+        except ValueError:
+            print(self.tag_str)
+            self.true_column['tag'] = "エラー"
+        try:
+            self.true_column['description'] = self.descriptions
+        except AttributeError:
+            print("エラー")
+            print(self.descriptions)
+            self.true_column['description'] = "エラー"
+        except ValueError:
+            print(self.descriptions)
+            self.true_column['description'] = "エラー"
+        try:
+            self.true_column['like'] = self.likes
+        except ValueError:
+            self.likes = "非表示"
+            self.true_column['like'] = self.likes
+        try:
+            self.true_column['dislike'] = self.dislikes
+        except ValueError:
+            self.dislikes = "非表示"
+            self.true_column['dislike'] = self.dislikes
+        pd.DataFrame(self.true_column).to_csv(self.scarch_videos_list_csv_file_path, mode='a', header=False, index=False)
+        print(self.scarch_videos_list_csv_file_path+"に追記しました")
+
+
+    def scarch_videos_list_csv_duplicate(self):
+        df = pd.read_csv(self.scarch_videos_list_csv_file_path, engine='python')
+        df_drop_duplicate = df.drop_duplicates(subset='channel_url', keep='last')
+        pd.DataFrame(df_drop_duplicate).to_csv(self.scarch_videos_list_csv_file_path,index=False)
+        print(self.scarch_videos_list_csv_file_path+"の重複削除しました")
 
 
 if __name__ == "__main__":
